bfb03af700b3bcbd0f4714b406ba7d9f8fb7c7d8
nahidsultan19 <nahidsultan38@gmail.com>
2022-07-18 22:05:41 +0600

    add redirects file

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Meals from '../Meals/Meals';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home page.</h2>
+            <Meals />
         </div>
     );
 };
